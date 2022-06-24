e24672ba551c265b0da88d136f60c7202807add2
Sumit Bagla <sumit.bagla@gmail.com>
2022-06-24 17:29:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('FruityVice fruity advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+
+# Convert json response into a flat table
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# load into a pandas dataframe
+streamlit.dataframe(fruityvice_normalized)
